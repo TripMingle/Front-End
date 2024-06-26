4d6a9a9d61b1b6f56854d5928e93ff2683628b36
minji kim <kmj_1027@naver.com>
2024-06-26 15:54:25 +0900

    chore(state) : zustand 설치 및 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import Image from "next/image";
 import * as styles from "./globalTheme.css";
 import { style } from "@vanilla-extract/css";
-
+import { create } from "zustand";
+import { combine } from "zustand/middleware";
 
 export default function Home() {
+    const useBearStore = create(
+        combine({ bears: 0 }, (set) => ({
+            increase: (by: number) =>
+                set((state) => ({ bears: state.bears + by })),
+        }))
+    );
+
+    const BearCounter = () => {
+        const bear = useBearStore((state) => state.bears);
+        return <h1>Bear : {bear}</h1>;
+    };
+
+    const BearIncrease = () => {
+        const increase = useBearStore((state) => state.increase);
+        return <button onClick={() => increase(5)}>up</button>;
+    };
+
     return (
         <main>
             <div>안녕하세용 i</div>
+            <BearCounter />
+            <BearIncrease />
         </main>
     );
 }
