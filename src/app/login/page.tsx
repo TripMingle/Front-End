21764577279375b7127bb427425c4f49b090754b
minji kim <kmj_1027@naver.com>
2024-07-31 18:37:00 +0900

    [feat/SWM-220] feat(로그인) : 로그인 페이지 개발

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,31 @@
 import '@/styles/font.css';
 import NonProfileHeader from '@/components/header/NonProfileHeader';
-import { pageContainer } from '@/styles/country/page.css';
+import * as styles from '@/styles/login/page.css';
+import Image from 'next/image';
 
 const Page = () => {
   return (
     <>
       <NonProfileHeader />
-      <div className={pageContainer}></div>
+      <div className={styles.pageContainer}>
+        <p className={styles.logo}>TripMingle</p>
+        <span className={styles.explain}>
+          로그인을 통해 여행을 즐겨보세용가리
+        </span>
+        <button className={styles.kakaoButtonContainer}>
+          <Image
+            src="/icons/kakaoIcon.svg"
+            alt="kakaoIcon"
+            width={18}
+            height={16}
+          />
+          <span className={styles.kakaoLoginText}>카카오로 간편 로그인</span>
+        </button>
+        <div className={styles.signupContainer}>
+          <span className={styles.text}>회원이 아니라면 ? </span>
+          <button className={styles.signupButton}>회원가입</button>
+        </div>
+      </div>
     </>
   );
 };
