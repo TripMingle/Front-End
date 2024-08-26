9360fbea57004dc8d7ff749667588a9f58a61f56
minji kim <kmj_1027@naver.com>
2024-08-26 14:00:16 +0900

    refactor(로그인) : 유저 정보 Cookie에 저장, useStore persist해제, cookie에서 값 불러오기

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
+import Cookies from 'js-cookie';
 
 type userState = {
   isLoggedIn: boolean;
@@ -9,21 +10,19 @@ type userState = {
   logout: () => void;
 };
 
-export const useUserStore = create<userState>()(
-  persist(
-    (set) => ({
-      isLoggedIn: false,
-      imageUrl: '',
-      nickName: '',
-      login: (imageUrl: string, nickName: string) =>
-        set({ isLoggedIn: true, imageUrl, nickName }),
-      logout: () => {
-        set({ isLoggedIn: false, imageUrl: '', nickName: '' });
-      },
-    }),
-    {
-      name: 'user-store',
-      storage: createJSONStorage(() => sessionStorage),
-    },
-  ),
-);
+export const useUserStore = create<userState>()((set) => ({
+  isLoggedIn: !!Cookies.get('access_token'),
+  imageUrl: Cookies.get('imageUrl') || '',
+  nickName: Cookies.get('nickName') || '',
+  login: (imageUrl: string, nickName: string) => {
+    Cookies.set('imageUrl', imageUrl);
+    Cookies.set('nickName', nickName);
+    set({ isLoggedIn: true, imageUrl, nickName });
+  },
+  logout: () => {
+    Cookies.remove('access_token');
+    Cookies.remove('imageUrl');
+    Cookies.remove('nickName');
+    set({ isLoggedIn: false, imageUrl: '', nickName: '' });
+  },
+}));
