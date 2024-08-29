8da8e953cb70eaf4178b65ff206b09d1e6f6ddb2
minji kim <kmj_1027@naver.com>
2024-08-29 20:18:10 +0900

    fix : user정보 초기화하는 Initializer 컴포넌트 생성,  app 최상위 layout에서 user정보 initialize

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 import Cookies from 'js-cookie';
-import { persist } from 'zustand/middleware';
 
 type userState = {
   isLoggedIn: boolean;
@@ -11,36 +10,26 @@ type userState = {
   logout: () => void;
 };
 
-export const useUserStore = create<userState>()(
-  persist(
-    (set) => ({
-      isLoggedIn: false,
-      imageUrl: '',
-      nickName: '',
-      initialize: () => {
-        set({
-          isLoggedIn: !!Cookies.get('access_token'),
-          imageUrl: Cookies.get('imageUrl') || '',
-          nickName: Cookies.get('nickName') || '',
-        });
-      },
-      login: (imageUrl: string, nickName: string) => {
-        Cookies.set('imageUrl', imageUrl);
-        Cookies.set('nickName', nickName);
-        set({ isLoggedIn: true, imageUrl, nickName });
-      },
-      logout: () => {
-        Cookies.remove('access_token');
-        Cookies.remove('imageUrl');
-        Cookies.remove('nickName');
-        set({ isLoggedIn: false, imageUrl: '', nickName: '' });
-      },
-    }),
-    {
-      name: 'use-user-store',
-      onRehydrateStorage: () => (state) => {
-        state?.initialize();
-      },
-    },
-  ),
-);
+export const useUserStore = create<userState>()((set) => ({
+  isLoggedIn: false,
+  imageUrl: '',
+  nickName: '',
+  initialize: () => {
+    set({
+      isLoggedIn: !!Cookies.get('access_token'),
+      imageUrl: Cookies.get('imageUrl') || '',
+      nickName: Cookies.get('nickName') || '',
+    });
+  },
+  login: (imageUrl: string, nickName: string) => {
+    Cookies.set('imageUrl', imageUrl);
+    Cookies.set('nickName', nickName);
+    set({ isLoggedIn: true, imageUrl, nickName });
+  },
+  logout: () => {
+    Cookies.remove('access_token');
+    Cookies.remove('imageUrl');
+    Cookies.remove('nickName');
+    set({ isLoggedIn: false, imageUrl: '', nickName: '' });
+  },
+}));
