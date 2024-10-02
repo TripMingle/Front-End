0b0e6e9f077a03e122ea9f349f06397cfcbad8b3
minji kim <kmj_1027@naver.com>
2024-10-02 13:19:49 +0900

    refactor(user) : 회원가입, 로그인, 정보받아오기 완료

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,63 +1,4 @@
-import {
-  getKakaoAuthorization,
-  getRefreshToken,
-  getToken,
-  removeKakaoAuthorization,
-  storeKakaoAuthorization,
-  storeToken,
-} from '@/utils/token';
-// import apiFetch from '.';
-
-// export const acquireAccessToken = async (code: string) => {
-//   const params = {
-//     code: code,
-//   };
-//   return apiFetch().get(`/kakao/callback`, false, params);
-// };
-
-export const kakaoSignup = async (
-  nickName: string,
-  nationality: string,
-  selfIntroduction: string,
-) => {
-  try {
-    const kakaoAuthorization = getKakaoAuthorization();
-    const data = {
-      nickName,
-      nationality,
-      selfIntroduction,
-    };
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/kakao/join`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Kakao-Authorization': `${kakaoAuthorization}`,
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (!res.ok) {
-      const data = await res.json();
-      if (res.status === 409 && data.code === 'U003') {
-        return data.message;
-      } else {
-        throw new Error('회원가입 실패');
-      }
-    }
-
-    const accessToken = res.headers.get('access-token');
-    const refreshToken = res.headers.get('refresh-token');
-
-    if (accessToken && refreshToken) {
-      storeToken(accessToken.replace('Bearer ', ''), refreshToken);
-      removeKakaoAuthorization();
-    }
-
-    return res.json();
-  } catch (error) {
-    console.error(error);
-  }
-};
+import { getFetch, postFetch } from '.';
 
 export const kakaoLogin = async (code: string) => {
   try {
@@ -71,64 +12,39 @@ export const kakaoLogin = async (code: string) => {
   }
 };
 
-export const checkNickName = async (nickName: string) => {
-  const params = {
-    nickName: `${nickName}`,
-  };
-  return;
-  // return apiFetch().get('/auth/validate/duplication', false, params);
+export const kakaoLogout = async () => {
+  return postFetch(`/user/logout`);
 };
 
-export const kakaoLogout = async () => {
+export const kakaoSignup = async (
+  nickName: string,
+  nationality: string,
+  selfIntroduction: string,
+) => {
   try {
-    const token = getToken();
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
+    const data = {
+      nickName,
+      nationality,
+      selfIntroduction,
+    };
+
+    const res = await fetch('/api/user/signup', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
+      body: JSON.stringify(data),
     });
 
-    if (!res.ok) throw new Error('로그아웃 실패');
-    return res.ok;
+    if (typeof res == 'string') return res;
+
+    return res.json();
   } catch (error) {
     console.error(error);
   }
+};
+
+export const checkNickName = async (nickName: string) => {
+  return getFetch(`/user/signup/validate?nickname=${nickName}`);
 };
 
 export const getUserInfo = async () => {
-  try {
-    const res = await fetch(`/auth/logout`);
-    if (!res.ok) throw new Error('유저 정보 받아오기 실패');
-
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
+  return getFetch(`/user/info`);
 };
-
-// export const updateAccessToken = async () => {
-//   try {
-//     const token = getRefreshToken();
-//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`, {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Refresh: `${token}`,
-//       },
-//     });
-
-//     if (!res.ok) throw new Error('액세스 토큰 재발급 실패');
-//     const accessToken = res.headers.get('access-token');
-//     const refreshToken = res.headers.get('refresh-token');
-
-//     if (accessToken && refreshToken) {
-//       storeToken(accessToken.replace('Bearer ', ''), refreshToken);
-//     }
-//     return accessToken || '';
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
---
+++ src/app/api/user/logout/route.ts
@@ -0,0 +1,24 @@
+import { getAccessToken } from '@/utils/server/token';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  const baseurl = `${process.env.NEXT_PUBLIC_API_URL}`;
+  const pathname = `/auth/logout`;
+  let token = await getAccessToken();
+
+  if (!token)
+    return NextResponse.json(
+      { error: 'access token이 없거나 만료되었습니다.' },
+      { status: 500 },
+    );
+
+  return await fetch(`${baseurl}${pathname}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    // @ts-ignore -- 연결이 단방향임을 나타냄
+    duplex: 'half',
+  });
+};
