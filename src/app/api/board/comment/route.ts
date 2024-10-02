ebdf55129dad1c9ec6859dd581774f4497456c22
minji kim <kmj_1027@naver.com>
2024-10-02 13:20:17 +0900

    refactor(board) : post 요청 시 액세스토큰 없으면 오류 던지기

--- src/app/api/board/comment/route.ts
+++ src/app/api/board/comment/route.ts
@@ -1,12 +1,16 @@
 import { getAccessToken } from '@/utils/server/token';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const POST = async (req: NextRequest) => {
   const baseurl = `${process.env.NEXT_PUBLIC_API_URL}`;
   const pathname = `/board/comment`;
   const body = req.body;
-  let accesstoken = await getAccessToken();
-  let token = accesstoken?.value || process.env.ACCESS_TOKEN;
+  let token = await getAccessToken();
+  if (!token)
+    return NextResponse.json(
+      { error: 'access token이 없거나 만료되었습니다.' },
+      { status: 500 },
+    );
 
   return await fetch(`${baseurl}${pathname}`, {
     method: 'POST',
--- src/app/api/board/route.ts
+++ src/app/api/board/route.ts
@@ -1,5 +1,5 @@
 import { getAccessToken } from '@/utils/server/token';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const GET = async (req: NextRequest) => {
   const baseurl = `${process.env.NEXT_PUBLIC_API_URL}`;
@@ -21,8 +21,12 @@ export const POST = async (req: NextRequest) => {
   const baseurl = `${process.env.NEXT_PUBLIC_API_URL}`;
   const pathname = `/board`;
   const body = req.body;
-  let accesstoken = await getAccessToken();
-  let token = accesstoken?.value || process.env.ACCESS_TOKEN;
+  let token = await getAccessToken();
+  if (!token)
+    return NextResponse.json(
+      { error: 'access token이 없거나 만료되었습니다.' },
+      { status: 500 },
+    );
 
   return await fetch(`${baseurl}${pathname}`, {
     method: 'POST',
