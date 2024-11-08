e7303e126ace22ac9d4618d226bc993b5751ac78
minji kim <kmj_1027@naver.com>
2024-11-08 20:42:05 +0900

    fix(TextBox) : TextBox -> NameBox, IntroductionBox로 분리

---
+++ src/components/signup/NameBox.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { UserSignupType } from '@/types/country/user';
+import { useFormContext } from 'react-hook-form';
+import FormField from './FormField';
+import {
+  container,
+  nickNameButton,
+  nickNamefieldContainer,
+  nickNameResult,
+} from '@/styles/signup/page.css';
+import { useEffect, useState } from 'react';
+
+const NameBox = () => {
+  const [isAvailable, setIsAvailable] = useState<boolean>(false);
+  const [result, setResult] = useState<string>('');
+  const { watch, register } = useFormContext<UserSignupType>();
+  // 닉네임 특수문자나 다른 공백 확인용
+  const regex = /^[가-힣a-zA-Z]+$/;
+  const name = watch('name');
+
+  useEffect(() => {
+    if (name && !regex.test(name)) {
+      setIsAvailable(false);
+      setResult('이름이 유효하지 않습니다.');
+    } else {
+      setResult(''); // 유효한 경우 메시지 초기화
+    }
+  }, [name]);
+
+  return (
+    <div className={nickNamefieldContainer}>
+      <FormField title="닉네임" required={true}>
+        <input
+          className={container({ select: true })}
+          type="text"
+          placeholder="이름을 입력하세요."
+          tabIndex={1}
+          {...register('name', { required: true })}
+        />
+      </FormField>
+      <span className={nickNameResult({ result: isAvailable })}>{result}</span>
+    </div>
+  );
+};
+
+export default NameBox;
