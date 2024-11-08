bd7a818ebabd33787138ef3f289358cbce69ccd7
minji kim <kmj_1027@naver.com>
2024-11-08 19:09:16 +0900

    feat(types/user) : UserSignupType 생성

--- src/types/country/user.ts
+++ src/types/country/user.ts
@@ -16,3 +16,23 @@ export type BoardCardProps = {
   maxCount: number;
   user: User;
 };
+
+export type UserSignupType = {
+  name: string;
+  gender: string;
+  birthDay: string;
+  phoneNumber: string;
+  nickName: string;
+  nationality: string;
+  selfIntroduction: string;
+};
+
+export const UserSingupDefault = {
+  name: '',
+  gender: '',
+  birthDay: '',
+  phoneNumber: '',
+  nickName: '',
+  nationality: '',
+  selfIntroduction: '',
+};
