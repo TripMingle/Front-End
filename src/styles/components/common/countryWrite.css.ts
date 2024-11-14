d2a8f8cbe6577631bcc404315179ab2a2a894e21
minji kim <kmj_1027@naver.com>
2024-11-14 19:34:47 +0900

    fix(CountryWrite) : sticky로 변경, 쓸데없는 스타일 삭제

--- src/styles/components/common/countryWrite.css.ts
+++ src/styles/components/common/countryWrite.css.ts
@@ -1,10 +1,8 @@
 import { vars } from '@/styles/globalTheme.css';
 import { style } from '@vanilla-extract/css';
-import { recipe } from '@vanilla-extract/recipes';
 
 export const countryWriteContainer = style({
-  position: 'fixed',
-  right: 10,
+  position: 'sticky',
   bottom: 20,
   display: 'flex',
   flexDirection: 'column',
@@ -12,36 +10,7 @@ export const countryWriteContainer = style({
   cursor: 'pointer',
   '@media': {
     'screen and (min-width: 1024px)': {
-      right: 20,
       bottom: 60,
-    },
-    'screen and (min-width: 1200px)': {
-      right: 'calc((100vw - 1200px)/2)',
-      bottom: 60,
-    },
-  },
-});
-
-export const writeText = style({
-  display: 'none',
-  '@media': {
-    'screen and (min-width: 1024px)': {
-      display: 'flex',
-      color: vars.color.secondary,
-      fontWeight: 500,
-      fontSize: 14,
-    },
-  },
-});
-
-export const writeItemContainer = style({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: 14,
-  alignItems: 'center',
-  '@media': {
-    'screen and (min-width: 1024px)': {
-      gap: 20,
     },
   },
 });
@@ -50,6 +19,8 @@ export const iconContainer = style({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+  marginLeft: 'auto',
+  marginRight: 10,
   width: 42,
   height: 42,
   zIndex: 10,
@@ -59,121 +30,10 @@ export const iconContainer = style({
     'screen and (min-width: 1024px)': {
       width: 48,
       height: 48,
+      marginRight: 20,
+    },
+    'screen and (min-width: 1200px)': {
+      marginRight: 'calc((100vw - 1200px)/2)',
     },
   },
 });
-
-export const icon = style({});
-
-export const writeContainer = recipe({
-  base: {
-    position: 'absolute',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 36,
-    height: 36,
-    zIndex: 3,
-    borderRadius: '50%',
-    background: vars.color.white,
-    fontFamily: vars.font.menu,
-    fontWeight: 'normal',
-    fontSize: 10,
-    color: vars.color.secondary,
-    transition: 'all 0.5s ease',
-    '@media': {
-      'screen and (min-width: 1024px)': {
-        fontSize: 12,
-        width: 42,
-        height: 42,
-      },
-    },
-  },
-  variants: {
-    active: {
-      true: {
-        border: `1px solid rgba(54, 136, 255, 0.2)`,
-        boxShadow: `0px 5px 10px 0px rgba(54, 136, 255, 0.3)`,
-      },
-      false: { bottom: 0 },
-    },
-    type: {
-      board: {
-        bottom: 52,
-        '@media': {
-          'screen and (min-width: 1024px)': {
-            bottom: 84,
-          },
-        },
-      },
-      post: {
-        bottom: 100,
-        '@media': {
-          'screen and (min-width: 1024px)': {
-            bottom: 140,
-          },
-        },
-      },
-    },
-  },
-  compoundVariants: [
-    {
-      variants: {
-        active: false,
-        type: 'board',
-      },
-      style: {
-        bottom: 0,
-        '@media': {
-          'screen and (min-width: 1024px)': {
-            bottom: 20,
-          },
-        },
-      },
-    },
-    {
-      variants: {
-        active: false,
-        type: 'post',
-      },
-      style: {
-        bottom: 0,
-        '@media': {
-          'screen and (min-width: 1024px)': {
-            bottom: 20,
-          },
-        },
-      },
-    },
-    {
-      variants: {
-        active: true,
-        type: 'board',
-      },
-      style: {
-        bottom: 52,
-        '@media': {
-          'screen and (min-width: 1024px)': {
-            bottom: 84,
-          },
-        },
-      },
-    },
-    {
-      variants: {
-        active: true,
-        type: 'post',
-      },
-      style: {
-        bottom: 100,
-        '@media': {
-          'screen and (min-width: 1024px)': {
-            bottom: 140,
-          },
-        },
-      },
-    },
-  ],
-});
-
-export const postWriteContainer = style({});
