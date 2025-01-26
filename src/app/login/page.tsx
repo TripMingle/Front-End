a0fd65fce09a04fbf4b71a2c5ae4df7cab522566
minji kim <kmj_1027@naver.com>
2025-01-27 03:27:42 +0900

    refactor(login): login버튼 클릭 시 signup페이지로 이동

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,21 @@
-'use client';
 import * as styles from '@/styles/login/page.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Page = () => {
-  const kakaoClickHandler = async () => {
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${window.location.origin}/login/kakao&response_type=code`;
-    window.location.replace(KAKAO_AUTH_URL);
-  };
+  // TODO: 백엔드 배포되면 KAKAO_AUTH_URL이동으로 수정
+  // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${window.location.origin}/login/kakao&response_type=code`;
 
   return (
     <>
       <div className={styles.pageContainer}>
         <p className={styles.logo}>TripMingle</p>
         <span className={styles.explain}>로그인하고 여행을 계속해요</span>
-        <button
+        <Link
           className={styles.kakaoButtonContainer}
-          onClick={kakaoClickHandler}
+          key="Signup"
+          href="/signup"
+          replace
         >
           <Image
             src="/icons/kakaoIcon.svg"
@@ -24,7 +24,7 @@ const Page = () => {
             height={16}
           />
           <span className={styles.kakaoLoginText}>카카오로 간편 로그인</span>
-        </button>
+        </Link>
       </div>
     </>
   );
