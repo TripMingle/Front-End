97ac3c88a66ffb8888a9056759bf6a2a7db543c8
minji kim <kmj_1027@naver.com>
2025-01-27 17:05:19 +0900

    refactor(CateogrySection): preview title, explain 부분 컴포넌트로 추출

---
+++ src/components/country/CategorySection.tsx
@@ -0,0 +1,71 @@
+import * as styles from '@/styles/country/page.css';
+import { ReactNode } from 'react';
+import Person from './Icons/Person';
+import House from './Icons/House';
+import Good from './Icons/Good';
+import Calendar from './Icons/Calendar';
+import More from '../common/More';
+
+type CategoryProps = {
+  category: string;
+  children: ReactNode;
+};
+
+const CategorySection = async ({ category, children }: CategoryProps) => {
+  return (
+    <div className={styles.categoryContainer}>
+      {categoryInfo[category].icon}
+      <span className={styles.category}>{categoryInfo[category].title}</span>
+      <div className={styles.textContainer}>
+        <span className={styles.explain}>{categoryInfo[category].explain}</span>
+        <More
+          path={categoryInfo[category].path}
+          category={categoryInfo[category].category}
+        />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default CategorySection;
+
+const categoryInfo: Record<
+  string,
+  {
+    icon: ReactNode;
+    title: string;
+    explain: string;
+    path: string;
+    category: string;
+  }
+> = {
+  board: {
+    icon: <Person className={styles.icon} width={36} height={36} />,
+    title: '여행 동행글',
+    explain: '전 세계 친구들과 자유롭게 동행해 보세요!',
+    path: '/board',
+    category: '',
+  },
+  rentalHome: {
+    title: '숙소 추천',
+    explain: '여행에 쌓인 피로도 풀어야죠!',
+    icon: <House className={styles.icon} width={36} height={36} />,
+    path: '/post',
+    category: 'RENTAL_HOME',
+  },
+  restaurant: {
+    title: '맛집 추천',
+    explain: '여행객들이 엄선한 맛집! 실패는 NO!',
+    icon: <Good className={styles.icon} width={36} height={36} />,
+    path: '/post',
+    category: 'RESTAURANT',
+  },
+  schedule: {
+    title: '일정 추천',
+    explain: '여행 계획 막막하신가요? 따라만 오세요!',
+    icon: <Calendar className={styles.icon} width={36} height={36} />,
+    path: '/post',
+    category: 'SCHEDULE',
+  },
+};
