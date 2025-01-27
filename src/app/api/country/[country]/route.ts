8d023603329b20f210717c66c52d7ee4e71006c1
minji kim <kmj_1027@naver.com>
2025-01-27 17:03:58 +0900

    refactor(mock): boardPreview, postPreview, countryInfo에 대한 mock data 생성

--- src/app/api/country/[country]/route.ts
+++ src/app/api/country/[country]/route.ts
@@ -1,8 +1,15 @@
 import { baseurl, withApiHandler } from '@/utils/server/api';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { mockCountryData } from '../../mock';
 
 export const GET = withApiHandler(
   async (req: NextRequest, config: RequestInit) => {
+    if (process.env.TEST) {
+      return NextResponse.json({
+        data: mockCountryData,
+      });
+    }
+
     const pathname = req.nextUrl.pathname.slice(4);
     return await fetch(`${baseurl}${pathname}`, {
       ...config,
--- src/app/api/post/preview/route.ts
+++ src/app/api/post/preview/route.ts
@@ -1,11 +1,27 @@
 import { baseurl, withApiHandler } from '@/utils/server/api';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import {
+  mockRentalHomePreviewData,
+  mockRestaurantPreviewData,
+  mockSchedulePreviewData,
+} from '../../mock';
 
 export const GET = withApiHandler(
   async (req: NextRequest, config: RequestInit) => {
     const pathname = '/postings/preview';
     const country = req.nextUrl.searchParams.get('country');
     const postingType = req.nextUrl.searchParams.get('postingType');
+
+    if (process.env.TEST) {
+      switch (postingType) {
+        case 'rentalHome':
+          return NextResponse.json({ data: mockRentalHomePreviewData });
+        case 'schedule':
+          return NextResponse.json({ data: mockSchedulePreviewData });
+        case 'restaurant':
+          return NextResponse.json({ data: mockRestaurantPreviewData });
+      }
+    }
 
     return await fetch(
       `${baseurl}${pathname}?country=${country}&postingType=${postingType}`,
