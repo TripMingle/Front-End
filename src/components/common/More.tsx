9eedf5e19c1b899a4e8e1bbdbcc9691f2675cfe8
minji kim <kmj_1027@naver.com>
2025-01-31 16:57:30 +0900

    refactor([country]): category없애고, post 더보기 클릭 시 searchParams 이용

--- src/components/common/More.tsx
+++ src/components/common/More.tsx
@@ -1,17 +1,15 @@
 'use client';
-import { usePostStore } from '@/store/postStore';
 import { more } from '@/styles/country/page.css';
 import { usePathname, useRouter } from 'next/navigation';
 
-const More = ({ path, category }: { path: string; category: string }) => {
+const More = ({ path }: { path: string }) => {
   const router = useRouter();
   const pathname = usePathname();
-  const setCategory = usePostStore((state) => state.setCategory);
 
   const clickHandler = () => {
-    if (path !== '/board') setCategory(category);
     router.push(pathname + path);
   };
+
   return (
     <span className={more} onClick={clickHandler}>
       {'더보기 > '}
--- src/components/country/CategorySection.tsx
+++ src/components/country/CategorySection.tsx
@@ -18,10 +18,7 @@ const CategorySection = async ({ category, children }: CategoryProps) => {
       <span className={styles.category}>{categoryInfo[category].title}</span>
       <div className={styles.textContainer}>
         <span className={styles.explain}>{categoryInfo[category].explain}</span>
-        <More
-          path={categoryInfo[category].path}
-          category={categoryInfo[category].category}
-        />
+        <More path={categoryInfo[category].path} />
       </div>
       {children}
     </div>
@@ -37,7 +34,6 @@ const categoryInfo: Record<
     title: string;
     explain: string;
     path: string;
-    category: string;
   }
 > = {
   board: {
@@ -45,27 +41,23 @@ const categoryInfo: Record<
     title: '여행 동행글',
     explain: '전 세계 친구들과 자유롭게 동행해 보세요!',
     path: '/board',
-    category: '',
   },
   rentalHome: {
     title: '숙소 추천',
     explain: '여행에 쌓인 피로도 풀어야죠!',
     icon: <House className={styles.icon} width={36} height={36} />,
-    path: '/post',
-    category: 'RENTAL_HOME',
+    path: '/post?category=rentalHome',
   },
   restaurant: {
     title: '맛집 추천',
     explain: '여행객들이 엄선한 맛집! 실패는 NO!',
     icon: <Good className={styles.icon} width={36} height={36} />,
-    path: '/post',
-    category: 'RESTAURANT',
+    path: '/post?category=restaurant',
   },
   schedule: {
     title: '일정 추천',
     explain: '여행 계획 막막하신가요? 따라만 오세요!',
     icon: <Calendar className={styles.icon} width={36} height={36} />,
-    path: '/post',
-    category: 'SCHEDULE',
+    path: '/post?category=schedule',
   },
 };
