afb277ab51a3900afe36b575362ec5610de44580
minji kim <kmj_1027@naver.com>
2025-02-03 18:20:50 +0900

    refactor(CategorySection): CategorySection includes Suspense

--- src/app/[country]/page.tsx
+++ src/app/[country]/page.tsx
@@ -2,9 +2,6 @@ import * as styles from '@/styles/country/page.css';
 import BoardPreview from '@/components/country/BoardPreview';
 import CountryBackground from '@/components/country/CountryBackground';
 import PostPreview from '@/components/country/PostPreivew';
-import { Suspense } from 'react';
-import BoardPreviewSkeleton from '@/components/country/BoardPreviewSkeleton';
-import PostPreviewSkeleton from '@/components/country/PostPreviewSkeleton';
 import CategorySection from '@/components/country/CategorySection';
 
 const Page = async ({ params }: { params: { country: string } }) => {
@@ -13,24 +10,16 @@ const Page = async ({ params }: { params: { country: string } }) => {
       <CountryBackground country={params.country} />
       <div className={styles.contentContainer}>
         <CategorySection category="board">
-          <Suspense fallback={<BoardPreviewSkeleton />}>
-            <BoardPreview country={params.country} />
-          </Suspense>
+          <BoardPreview country={params.country} />
         </CategorySection>
         <CategorySection category="restaurant">
-          <Suspense fallback={<PostPreviewSkeleton />}>
-            <PostPreview country={params.country} category="restaurant" />
-          </Suspense>
+          <PostPreview country={params.country} category="restaurant" />
         </CategorySection>
         <CategorySection category="rentalHome">
-          <Suspense fallback={<PostPreviewSkeleton />}>
-            <PostPreview country={params.country} category="rentalHome" />
-          </Suspense>
+          <PostPreview country={params.country} category="rentalHome" />
         </CategorySection>
         <CategorySection category="schedule">
-          <Suspense fallback={<PostPreviewSkeleton />}>
-            <PostPreview country={params.country} category="schedule" />
-          </Suspense>
+          <PostPreview country={params.country} category="schedule" />
         </CategorySection>
       </div>
     </>
--- src/components/country/CategorySection.tsx
+++ src/components/country/CategorySection.tsx
@@ -1,10 +1,12 @@
 import * as styles from '@/styles/country/page.css';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 import Person from './Icons/Person';
 import House from './Icons/House';
 import Good from './Icons/Good';
 import Calendar from './Icons/Calendar';
 import More from '../common/More';
+import BoardPreviewSkeleton from './BoardPreviewSkeleton';
+import PostPreviewSkeleton from './PostPreviewSkeleton';
 
 type CategoryProps = {
   category: string;
@@ -20,7 +22,7 @@ const CategorySection = async ({ category, children }: CategoryProps) => {
         <span className={styles.explain}>{categoryInfo[category].explain}</span>
         <More path={categoryInfo[category].path} />
       </div>
-      {children}
+      <Suspense fallback={categoryInfo[category].skeleton}>{children}</Suspense>
     </div>
   );
 };
@@ -31,6 +33,7 @@ const categoryInfo: Record<
   string,
   {
     icon: ReactNode;
+    skeleton: ReactNode;
     title: string;
     explain: string;
     path: string;
@@ -38,26 +41,30 @@ const categoryInfo: Record<
 > = {
   board: {
     icon: <Person className={styles.icon} width={36} height={36} />,
+    skeleton: <BoardPreviewSkeleton />,
     title: '여행 동행글',
     explain: '전 세계 친구들과 자유롭게 동행해 보세요!',
     path: '/board',
   },
   rentalHome: {
+    icon: <House className={styles.icon} width={36} height={36} />,
+    skeleton: <PostPreviewSkeleton />,
     title: '숙소 추천',
     explain: '여행에 쌓인 피로도 풀어야죠!',
-    icon: <House className={styles.icon} width={36} height={36} />,
     path: '/post?category=rentalHome',
   },
   restaurant: {
+    icon: <Good className={styles.icon} width={36} height={36} />,
+    skeleton: <PostPreviewSkeleton />,
     title: '맛집 추천',
     explain: '여행객들이 엄선한 맛집! 실패는 NO!',
-    icon: <Good className={styles.icon} width={36} height={36} />,
     path: '/post?category=restaurant',
   },
   schedule: {
+    icon: <Calendar className={styles.icon} width={36} height={36} />,
+    skeleton: <PostPreviewSkeleton />,
     title: '일정 추천',
     explain: '여행 계획 막막하신가요? 따라만 오세요!',
-    icon: <Calendar className={styles.icon} width={36} height={36} />,
     path: '/post?category=schedule',
   },
 };
