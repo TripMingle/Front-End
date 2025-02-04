c7ef31a99f9edf882cea012df3d0c473ec348823
minji kim <kmj_1027@naver.com>
2025-02-04 10:29:23 +0900

    feat(error): [country]/board/[id], [country]/post/[id] error page 추가

---
+++ src/app/[country]/board/[id]/error.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import ErrorComponent from '@/components/common/ErrorComponent';
+import { usePathname, useRouter } from 'next/navigation';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  console.error('Error occurred:', error);
+  const router = useRouter();
+  const pathname = usePathname();
+  const country = pathname.split('/')[1];
+
+  return (
+    <ErrorComponent
+      title="해당 게시물을 찾을 수 없습니다."
+      message=""
+      button="동행 게시글 보러가기"
+      handler={() => router.replace(`/${country}/board`)}
+    />
+  );
+}
